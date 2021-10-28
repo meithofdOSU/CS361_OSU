463a7230433c26576f0af29179774e3de87d3a1e
meithofdOSU <91294471+meithofdOSU@users.noreply.github.com>
2021-10-27 20:18:45 -0700

    Include note for future changes.

--- exoplanet.js
+++ exoplanet.js
@@ -1,8 +1,13 @@
+// This is the template for downloading the exoplanet data.
+// When my teammates complete their api, I will include a third optional parameter to download their data to csv.
+// This will allow me to pull their data to incorporate into my project.
+// This will read as the function signature '(url, dest, flag)'.
+
 const https = require('https');
 const fs = require('fs');
 
 const queryURL = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+*+from+pscomppars&format=csv";
-const dest = "./output.csv";
+const destination = "./output.csv";
 
 function download(url, dest) {
   return new Promise((resolve, reject) => {
@@ -41,4 +46,4 @@ function download(url, dest) {
   });
 };
 
-download(queryURL,dest);
+download(queryURL,destination);
